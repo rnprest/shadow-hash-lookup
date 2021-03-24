9996086be66469666d95d346ffb94c252f03ad47
Ryan Preston <r.preston73@gmail.com>
2021-03-23 21:12:01 -0400

    Use the shadow file usernames for html hacking
    Now, the following output is generated when printPasswords.py is run:
    
    User Password Security Report:
    rnprest: ❌ hello
    bob: ❌ 123
    jeremy: ✅ Unfound Hash
    
    The following users need to change their passwords:
    rnprest
    bob

--- printPasswords.py
+++ printPasswords.py
@@ -1,4 +1,5 @@
 import sys
+from HTMLFormHack import *
 
 # Error handling
 if len(sys.argv) != 2:
@@ -6,15 +7,39 @@ if len(sys.argv) != 2:
     \t$ python3 printPasswords.py shadowCopy"""
     sys.exit(errorMessage)
 
+userList = [] # Contains all users from the shadow file
+hashList = [] # Limit uses of decryptHash function call by combining all hashes
+needToChangePasswordList = []
 shadowFile = open(sys.argv[1], 'r')
 
+print('User Password Security Report:')
 for line in shadowFile:
     if '$6$' in line:
-        print(line)
         beforeHashIndex = line.find(':')
         afterHashIndex = line.find(':', beforeHashIndex+1)
         username = line[:beforeHashIndex]
         passwordHash = line[beforeHashIndex+4:afterHashIndex]
+        userList.append(username)
+        hashList.append(passwordHash)
 
-        print(f'The username is {username}')
-        print(f'The password hash is {passwordHash}')
+hashes = listToString(hashList)
+decryptedHashes = decryptHash(hashes)
+
+# Print whether each user's password hash was found
+for (user, decrypted) in zip(userList, decryptedHashes):
+    result = ''
+    greenCheck = '✅ '
+    redX = '❌ '
+    if (decrypted == "Unfound"):
+        result = greenCheck + 'Unfound Hash'
+    else:
+        result = redX + decrypted
+        needToChangePasswordList.append(user)
+    print(f'{user}: {result}')
+
+# Print the users that need to change their passwords
+print('\nThe following users need to change their passwords:')
+for user in needToChangePasswordList:
+    print(user)
+
+
