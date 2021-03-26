587db455a7510feb5789dd15f397737fa1adee27
Ryan Preston <r.preston73@gmail.com>
2021-03-26 11:03:31 -0400

    Read salted hashes from shadow file
    This program does indeed parse the correct hashes from the shadow file
    (tested by hardcoding them into HTMLFormHack.py), BUT we need to find a
    workaround for the hashes being salted. The md5decrypt database only
    stores unsalted hashes for obvious reasons, and we didn't account for
    the shadow file using salt.

--- printPasswords.py
+++ printPasswords.py
@@ -1,21 +1,46 @@
 import sys
 from HTMLFormHack import decryptHash, listToString
 
+# -----------------------------------------------------------------------------
+# Functions
+# -----------------------------------------------------------------------------
+
+# Parse the usernames and SHA512-encrypted passwords from the shadow file
+def getUsersAndPasswords(file):
+    users = [] # Contains all users from the shadow file
+    hashes = [] # Limit uses of decryptHash function call by combining all hashes
+    for line in file:
+        if '$6$' in line:
+            beforeHashIndex = line.find(':')
+            afterHashIndex = line.find(':', beforeHashIndex+1)
+            username = line[:beforeHashIndex]
+            passwordHash = line[beforeHashIndex+4:afterHashIndex]
+            users.append(username)
+            hashes.append(passwordHash)
+    return users, hashes
+
+# -----------------------------------------------------------------------------
+# Code
+# -----------------------------------------------------------------------------
+
 needToChangePasswordList = []
 
 # Check if shadow file exists
 try:
     shadowFile = open('/etc/shadow', 'r')
 except FileNotFoundError:
-    errorMessage = ("Error: Can't find/open shadow file.\n"
-    "If you know /etc/shadow exists, then run this program with privileges:\n"
-    "\t$ sudo python3 printPasswords.py")
+    errorMessage = "Error: Can't find shadow file"
     sys.exit(errorMessage)
 
 print('User Password Security Report:')
 
 userList, hashList = getUsersAndPasswords(shadowFile)
+print(f'userList = {userList}')
+print(f'hashList = {hashList}')
+
 hashes = listToString(hashList)
+print(f'\nthe hashes string = {hashes}')
+
 decryptedHashes = decryptHash(hashes)
 
 # Print whether each user's password hash was found
@@ -34,21 +59,3 @@ for (user, decrypted) in zip(userList, decryptedHashes):
 print('\nThe following users need to change their passwords:')
 for user in needToChangePasswordList:
     print(user)
-
-# -----------------------------------------------------------------------------
-# functions
-# -----------------------------------------------------------------------------
-
-# Parse the usernames and SHA512-encrypted passwords from the shadow file
-def getUsersAndPasswords (file):
-    users = [] # Contains all users from the shadow file
-    hashes = [] # Limit uses of decryptHash function call by combining all hashes
-    for line in file:
-        if '$6$' in line:
-            beforeHashIndex = line.find(':')
-            afterHashIndex = line.find(':', beforeHashIndex+1)
-            username = line[:beforeHashIndex]
-            passwordHash = line[beforeHashIndex+4:afterHashIndex]
-            users.append(username)
-            hashes.append(passwordHash)
-    return users, hashes
